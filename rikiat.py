8016d0b442003896dd4d6b4cf3f97262e13cc69a
rikiat <rikiatwhite@mailinator.com>
2017-07-19 20:08:39 -0400

    Add files via upload

---
+++ rikiat.py
@@ -0,0 +1,77 @@
+#print ("Hello World")
+#i will say hello
+
+'''
+this is a comment not a code
+print ("this is a code")
+a="0"
+b=2
+c=3
+print (a+str(b)+str(c))
+'''
+
+#test1 = int (input ("first lesson python"))
+#test2 = int (input ("how you doing"))
+#test3 = int (input ("what is your color"))
+#average = (test1+test2+test3)/3
+#print ("your average is",average)
+
+#print (3+4)
+#print (3-4)
+#print (3*4)
+#print (3/4)
+#print (3%2)
+#print (3**4)
+#print (3//4)
+#a=0
+#a+=2
+#print (a)
+#print ("My name is", end="")
+#print ("Rikiat")
+#print ("my name is", end=" ")
+#print ("Rikiat")
+
+#def main():
+ #   print("hello")
+  #  print ("everyone")
+#main()
+
+#def main():
+ #   average(5,7)
+#def average(a,b):
+ #   print ((a+b)/2)
+#main()
+'''
+a=15
+def someFunction():
+    a=10
+    print (a)
+someFunction()
+print (a)
+'''
+
+
+'''a = 30
+if a>=22:
+    print ("if")
+elif a >=21:
+    print ("elif")
+else:
+    print ("else")'''
+
+
+print ("what is the temperature outside?")
+a=75
+if a>=60 and a<90:
+    print ("out warm")
+elif a>=90:
+    print ("out hot")
+elif a<=60:
+    print ("out chilly")
+
+
+
+
+
+
+
