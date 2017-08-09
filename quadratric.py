57714fe49ee33f6db5321dab21e3f32766bb3cdb
rikiat <rikiatwhite@mailinator.com>
2017-08-09 17:34:16 -0400

    Add files via upload

---
+++ quadratric.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the coefficients of a: "))
+b = int(input("Enter the coefficients of b: "))
+c = int(input("Enter the coefficients of c: "))
+x = int(input("Enter the coefficients of x: "))
+d=a*(x*x)+(b*x)+c
+print(d)
